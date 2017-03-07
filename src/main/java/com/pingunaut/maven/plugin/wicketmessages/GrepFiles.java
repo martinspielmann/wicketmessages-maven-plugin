bb51631d466bcb756f7b8a9173cdaa0466481568
Martin Spielmann <martin.spielmann@pingunaut.com>
2017-03-07 23:42:35 +0100

    remove unused import

--- src/main/java/com/pingunaut/maven/plugin/wicketmessages/GrepFiles.java
+++ src/main/java/com/pingunaut/maven/plugin/wicketmessages/GrepFiles.java
@@ -9,7 +9,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.function.BiPredicate;
 
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugin.logging.SystemStreamLog;
 
 public class GrepFiles {
 
